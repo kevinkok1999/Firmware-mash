27e1aa78cfd3b329e6f7ae1b813eeb8f3bca08b7
kevinkok1999 <kevinkok1999@gmail.com>
2026-09-17 17:57:24 +0200

    test: run snapshot and journal host suites

--- test/run_host_tests.sh
+++ test/run_host_tests.sh
@@ -7,13 +7,20 @@ mkdir -p "$build_dir"
 
 cc_bin="${CC:-cc}"
 common_flags=(-std=c11 -Wall -Wextra -Werror -D_POSIX_C_SOURCE=200809L)
+include_flags=(-I"${repo_root}/components/mog_message_store/include")
 
-"$cc_bin" "${common_flags[@]}" \
-  -I"${repo_root}/components/mog_message_store/include" \
+"$cc_bin" "${common_flags[@]}" "${include_flags[@]}" \
   "${repo_root}/components/mog_message_store/mog_store_snapshot.c" \
   "${repo_root}/test/host/test_mog_store_snapshot.c" \
   -o "${build_dir}/test_mog_store_snapshot"
 
+"$cc_bin" "${common_flags[@]}" "${include_flags[@]}" \
+  "${repo_root}/components/mog_message_store/mog_store_snapshot.c" \
+  "${repo_root}/components/mog_message_store/mog_store_journal.c" \
+  "${repo_root}/test/host/test_mog_store_journal.c" \
+  -o "${build_dir}/test_mog_store_journal"
+
 "${build_dir}/test_mog_store_snapshot"
+"${build_dir}/test_mog_store_journal"
 
 echo "Firmware-mash host tests: PASS"
